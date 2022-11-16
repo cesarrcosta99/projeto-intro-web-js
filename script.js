067297c20628763f905a49faa242148d01b940bc
unknown <cesarrcosta99@gmail.com>
2022-11-16 19:40:06 -0300

    Correção exercicio 1

---
+++ script.js
@@ -0,0 +1,125 @@
+const cursos = [
+    {
+        curso: "HTML e CSS",
+        descricao: "Veja HTML E CSS do basico ao avançado",
+        duracao: "1 mês",
+        valor: 500
+    },
+    {
+        curso: "JavaScript",
+        descricao: "Veja JavaScript do basico ao avançado",
+        duracao: "2 meses",
+        valor: 900
+    },
+    {
+        curso: "APIsRest",
+        descricao: "Veja APIsRest do basico ao avançado",
+        duracao: "6 meses",
+        valor: 900
+    }
+]
+
+const turmas = [
+    {
+        turma: "Hipátia",
+        curso: "JavaScript",
+        inicio: "30/11/2022",
+        termino: "30/01/2023",
+        numeroDeAlunos: 150,
+        periodo: "noturno",
+        concluida: false
+    },
+    {
+        turma: "Sibyla",
+        curso: "JavaScript",
+        inicio: "30/10/2022",
+        termino: "30/12/2022",
+        numeroDelunos: 200,
+        periodo: "integral",
+        concluida: false
+    },
+    {
+        turma: "Curie",
+        curso: "HTML e CSS",
+        inicio: "15/09/2022",
+        termino: "15/10/2022",
+        numeroDeAlunos: 180,
+        periodo: "noturno",
+        concluida: true
+    },
+    {
+        turma: "Zhenyi",
+        curso: "HTML e CSS",
+        inicio: "01/11/2022",
+        termino: "01/11/2023",
+        numeroDeAlunos: 80,
+        periodo: "integral",
+        concluida: false
+    },
+    {
+        turma: "Clarke",
+        curso: "HTML e CSS",
+        inicio: "04/07/2022",
+        termino: "04/09/2022",
+        numeroDeAlunos: 200,
+        periodo: "noturno",
+        concluida: true
+    },
+    {
+        turma: "Blackwell",
+        curso: "APIsRest",
+        inicio: "20/03/2022",
+        termino: "20/06/2022",
+        numeroDeAlunos: 100,
+        periodo: "integral",
+        concluida: true
+    },
+    {
+        turma: "Elion",
+        curso: "APIsRest",
+        inicio: "12/01/2022",
+        termino: "12/06/2022",
+        numeroDeAlunos: 200,
+        periodo: "noturno",
+        concluida: true
+    },
+    {
+        turma: "Burnell",
+        curso: "APIsRest",
+        inicio: "18/10/2022",
+        termino: "18/04/2023",
+        numeroDeAlunos: 90,
+        periodo: "integral",
+        concluida: false
+    }
+]
+const estudantes=[
+    {
+      estudante:"Chris Evans",
+      turma:"Hipátia",
+      curso:"JavaScript",
+      valor:"900 reais",
+      nParcelas:9,
+      desconto:false,
+      parcelas:100
+    },
+    {
+      estudante:"Halle Berry",
+      turma:"Burnell",
+      curso:"APIsRest",
+      valor:"2000 reais",
+      nParcelas:4,
+      desconto:false,
+      parcelas:100
+    },
+    {
+      estudante:"Lashana Lynch",
+      turma:"Zhenyi",
+      curso:"HTML e CSS",
+      valor:"500 reais",
+      nParcelas:1,
+      desconto:true,
+      parcelas:500
+    }
+]
+
